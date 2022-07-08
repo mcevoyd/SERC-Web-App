905da3db66dc103ace715322696f0bab4114c99b
mcevoyd <darren.mc.evoy@hotmail.co.uk>
2022-07-08 15:46:50 +0100

    who knows anymore

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://dog.ceo/api/breeds/image', true);
+request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
