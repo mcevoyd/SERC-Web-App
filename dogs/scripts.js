e041f8610bb65f1b77380f1b81e3a2d06ede2817
mcevoyd <darren.mc.evoy@hotmail.co.uk>
2022-07-08 15:34:30 +0100

    again

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
+request.open('GET', 'https://dog.ceo/api/breeds/image', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
