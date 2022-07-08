c8319c578eca2b5246cd59581f3c5db892693710
mcevoyd <darren.mc.evoy@hotmail.co.uk>
2022-07-08 15:25:51 +0100

    hopefully this works

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -17,19 +17,25 @@ request.onload = function () {
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+      var images = data.message;
+      images.forEach(image => {
+        const card = document.createElement('div')
+        card.setAttribute('class', 'card')
 
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+        const h1 = document.createElement('h1');
+        h1.textContent = "Dog"
 
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+        const img = document.createElement('img')
+        img.src = '${image}'
+        
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(img);
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
+      })
+      
+
+      
     });
   } else {
     const errorMessage = document.createElement('marquee');
